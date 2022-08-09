c387ba49bb2a353f1d6ecb262f28e74c4d70b36c
Nikita Bogomolov <bogomolovnikita163@gmail.com>
2022-08-09 18:51:35 +0700

    create _menu

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+window.addEventListener('DOMContentLoaded', function() {
+    
+    // const link = document.querySelectorAll('.menu__link'),
+    //       subLink = document.querySelectorAll('.menu__sublink');
+
+    // link.forEach((item, i) => {
+        
+    // });
+
+});
